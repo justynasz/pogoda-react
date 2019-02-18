89208bed00d35c6ae395a2bbdbea29497b01b79b
justynasz <justynaszczygiel@outlook.com>
2019-02-18 23:59:10 +0100

    Updates

---
+++ precache-manifest.7ab2d31a5fc36651452e7c5b53242dc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec61bf6b182b233e46d4",
+    "url": "/pogoda-react/static/js/runtime~main.ec61bf6b.js"
+  },
+  {
+    "revision": "00fd10ca2d4fe7bcd335",
+    "url": "/pogoda-react/static/js/main.00fd10ca.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/pogoda-react/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "00fd10ca2d4fe7bcd335",
+    "url": "/pogoda-react/static/css/main.ab9797f8.chunk.css"
+  },
+  {
+    "revision": "b0343b7996c4de6ea5341346229af33b",
+    "url": "/pogoda-react/index.html"
+  }
+];
